735b90b248cedd37d50e13a330ecf8120c31553e
sigmapi-academy <kumarsatyendermishra@gmail.com>
2026-05-16 14:34:22 +0530

    Discussed Data types and operators in python

---
+++ Coding/Chapter0/zero.py
@@ -0,0 +1,10 @@
+# Variable amount is total spending on grocery 
+amount = 1000
+
+
+test1 = 10
+test2= 7
+test3 = 8
+finalTest = 18
+# Total marks is the sum of marks in all the tests of mathematics. 
+total_marks = test1 + test2 + test3 + finalTest
